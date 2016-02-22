d62cb728f3adcd1e1df0b3d503f47def30c54e04
Zoltan Kochan <zoltan.kochan@gmail.com>
2016-02-22 03:22:53 +0200

    code style update

--- test/index.js
+++ test/index.js
@@ -1,4 +1,7 @@
 'use strict'
+const describe = require('mocha').describe
+const it = require('mocha').it
+const beforeEach = require('mocha').beforeEach
 const chai = require('chai')
 const expect = chai.expect
 const sinon = require('sinon')
@@ -8,32 +11,36 @@ const request = require('supertest')
 
 chai.use(require('sinon-chai'))
 
-describe('auth', function() {
+describe('auth', function () {
   let server
 
-  beforeEach(function() {
+  beforeEach(function () {
     server = hexi()
     return server.register(hexiAuth)
   })
 
-  it('should throw exception if no auth middleware passed', function() {
+  it('should throw exception if no auth middleware passed', function () {
     expect(() => server.auth())
       .to.throw(Error, 'auth middleware should be a function')
   })
 
-  it('should throw exception if passed auth middleware is not function', function() {
-    expect(() => server.auth('not a function'))
-      .to.throw(Error, 'auth middleware should be a function')
-  })
+  it('should throw exception if passed auth middleware is not function',
+    function () {
+      expect(() => server.auth('not a function'))
+        .to.throw(Error, 'auth middleware should be a function')
+    }
+  )
 
-  it('should throw exception if auth middleware specified more than once', function() {
-    server.auth(function() {})
+  it('should throw exception if auth middleware specified more than once',
+    function () {
+      server.auth(function () {})
 
-    expect(() => server.auth(function() {}))
-      .to.throw(Error, 'auth middleware cannot be specified more than once')
-  })
+      expect(() => server.auth(function () {}))
+        .to.throw(Error, 'auth middleware cannot be specified more than once')
+    }
+  )
 
-  it('should disable authentication on a route', function(done) {
+  it('should disable authentication on a route', function (done) {
     server.auth((req, res, next) => res.status(401).end())
 
     server.route({
@@ -42,7 +49,7 @@ describe('auth', function() {
       config: {
         auth: false,
       },
-      handler(req, res) {
+      handler (req, res) {
         res.status(200).end()
       },
     })
@@ -52,7 +59,7 @@ describe('auth', function() {
       .expect(200, done)
   })
 
-  it('should forbid unauthenticated request', function(done) {
+  it('should forbid unauthenticated request', function (done) {
     server.auth((req, res, next) => {
       if (req.headers.foo === 'bar') next()
       res.status(401).end()
@@ -61,7 +68,7 @@ describe('auth', function() {
     server.route({
       path: '/',
       method: 'GET',
-      handler(req, res) {
+      handler (req, res) {
         res.status(200).end()
       },
     })
@@ -71,34 +78,36 @@ describe('auth', function() {
       .expect(401, done)
   })
 
-  it('should run the authentication middleware before the last middleware', function(done) {
-    const sessionMiddleware = sinon.spy((req, res, next) => next())
-    server.route.pre((next, opts) => {
-      next(Object.assign(opts, {
-        pre: opts.pre.concat(sessionMiddleware),
-      }))
-    })
-
-    const authMiddleware = sinon.spy((req, res, next) => next())
-    server.auth(authMiddleware)
-
-    server.route({
-      path: '/',
-      method: 'GET',
-      handler(req, res) {
-        res.status(200).end()
-      },
-    })
-
-    request(server.express)
-      .get('/')
-      .expect(401, () => {
-        expect(sessionMiddleware).to.have.been.calledBefore(authMiddleware)
-        done()
+  it('should run the authentication middleware before the last middleware',
+    function (done) {
+      const sessionMiddleware = sinon.spy((req, res, next) => next())
+      server.route.pre((next, opts) => {
+        next(Object.assign(opts, {
+          pre: opts.pre.concat(sessionMiddleware),
+        }))
       })
-  })
 
-  it('should allow authenticated request', function(done) {
+      const authMiddleware = sinon.spy((req, res, next) => next())
+      server.auth(authMiddleware)
+
+      server.route({
+        path: '/',
+        method: 'GET',
+        handler (req, res) {
+          res.status(200).end()
+        },
+      })
+
+      request(server.express)
+        .get('/')
+        .expect(401, () => {
+          expect(sessionMiddleware).to.have.been.calledBefore(authMiddleware)
+          done()
+        })
+    }
+  )
+
+  it('should allow authenticated request', function (done) {
     server.auth((req, res, next) => {
       if (req.headers.foo === 'bar') next()
       res.status(401).end()
@@ -107,7 +116,7 @@ describe('auth', function() {
     server.route({
       path: '/',
       method: 'GET',
-      handler(req, res) {
+      handler (req, res) {
         res.status(200).end()
       },
     })
